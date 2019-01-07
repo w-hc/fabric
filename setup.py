33e46f0e147f31918088ec40781c252afd1ef537
Haochen <whc@uchicago.edu>
2019-01-06 22:50:38 -0600

    fix package installation

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ttic_fabric',
-    version='0.1.0',
+    version='0.1.1',
     author='Haochen Wang',
     author_email='whc@uchicago.edu',
-    packages=['fabric'],
+    packages=find_packages(),
     python_requires='>=3',
     install_requires=[
+        # note that these requirements may not be exhaustive
+        'pyyaml'
         'numpy',
         'torch',
         'wget',
         'tensorboardX',
     ],
+    package_data={
+        # If any package contains *.yml, include them:
+        '': ['*.yml'],
+    },
     entry_points={
         'console_scripts': [
             'smit=fabric.cluster.submit:main',
@@ -20,5 +26,5 @@ setup(
             'bloom=fabric.cluster.bloom:main'
         ]
     },
-    zip_safe=False
+    zip_safe=False  # accessing config files without using pkg_resources.
 )
