d7aa8cff14b9912a90d5ced7724e26d80db86ea4
Haochen Wang <wanghaochen@uchicago.edu>
2020-03-16 11:19:46 -0500

    update gatherer

--- fabric/deploy/gather.py
+++ fabric/deploy/gather.py
@@ -2,6 +2,11 @@ import os
 import os.path as osp
 from collections import OrderedDict
 
+'''
+Experiment hierarchy
+    root -> subroot -> deployments
+'''
+
 
 class Gatherer():
     '''
@@ -17,20 +22,35 @@ class Gatherer():
         package_path = research_project_cls.__path__[0]
         # the exp dir is parallel to the package
         basedir = osp.dirname(package_path)
-        self.root = osp.join(basedir, exp_root)
+        self.root = basedir
+        self.exp_store_root = osp.join(basedir, exp_root)
+        self.deployed_in = 'runs'
+        self.where_to_look_within_each_exp = 'output'
 
-    def collect(
-        self, exp_name: str, processing_fn,
-        deployed_in='runs', where_to_look_within_each_exp='output'
-    ):
+    def _path_to_subroot(self, subroot: str):
+        return osp.join(self.exp_store_root, subroot)
+
+    def browse_subroot(self, subroot: str):
+        subroot_p = self._path_to_subroot(subroot)
+        deployed_p = osp.join(subroot_p, self.deployed_in)
+        return sorted(os.listdir(deployed_p))
+
+    def path_to_exp(self, subroot, exp):
+        path = self._path_to_subroot(subroot)
+        # .../ablate_different_arch/runs/resnet34
+        return osp.join(path, self.deployed_in, exp)
+
+    def collect(self, subroot: str, exp_name: str, processing_fn):
+        path = self.path_to_exp(subroot, exp_name)
+        path = osp.join(path, self.where_to_look_within_each_exp)
+        return processing_fn(path)
+
+    def all_collect(self, subroot: str, processing_fn):
         '''
-            exp_name: initial_trials/haul/
+            subroot: initial_trials/haul/
         '''
-        subroot = osp.join(self.root, exp_name, deployed_in)
-        exps = sorted(os.listdir(subroot))
+        exps = self.browse_subroot(subroot)
         accu = OrderedDict()
         for e in exps:
-            where2look = osp.join(subroot, e, where_to_look_within_each_exp)
-            res = processing_fn(where2look)
-            accu[e] = res
+            accu[e] = self.collect(subroot, e, processing_fn)
         return accu
