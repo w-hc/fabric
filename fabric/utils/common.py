999c94d29f1ff955d9d3e31c7620fe641579d53a
Haochen Wang <wanghaochen@uchicago.edu>
2020-04-14 14:07:53 -0400

    add common

---
+++ fabric/utils/common.py
@@ -0,0 +1,13 @@
+'''
+Some commonly used snippets
+'''
+
+
+def mapify_iterable(iter_of_dict, field_name):
+    """Convert an iterable of dicts into a big dict indexed by chosen field
+    I can't think of a better name. 'Tis catchy.
+    """
+    acc = dict()
+    for item in iter_of_dict:
+        acc[item[field_name]] = item
+    return acc
