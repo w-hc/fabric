967d48de33ddc0a1d5a273ca63d1c9087be6318d
Haochen Wang <wanghaochen@uchicago.edu>
2020-09-19 19:17:00 -0400

    change warning email to a context manager

--- fabric/utils/mailer.py
+++ fabric/utils/mailer.py
@@ -1,9 +1,11 @@
+import logging
 import functools
+from contextlib import contextmanager
 import traceback
 import smtplib
 from email.mime.text import MIMEText
-from fabric.utils.logging import setup_logging
-logger = setup_logging(__file__)
+
+logger = logging.getLogger(__name__)
 
 
 def send_email(subject, body, to):
@@ -14,48 +16,65 @@ def send_email(subject, body, to):
     s.sendmail('lab', to, mime.as_string())
 
 
-class ExceptionEmail:
-    """
-    A decorator that sends a warning email when catching
-    exceptions. The caught exception will be re-raised.
-    """
-    def __init__(self, subject, address):
-        """
-        Args:
-            subject: title of the email; some identifying metadata
-            address: mail recipient address
-        """
-        self.subject = subject
-        self.address = address
-
-    def __call__(self, f):
-        @functools.wraps(f)
-        def ret(*args, **kwargs):
-            try:
-                f(*args, **kwargs)
-            # except KeyboardInterrupt:
-            #     pass  # do not send email on sigint
-            except Exception as e:
-                logger.info(
-                    "Exception triggered. Emailing {}".format(self.address))
-                send_email(
-                    subject=self.subject,
-                    body=traceback.format_exc(),
-                    to=self.address)
-                raise(e)
-        return ret
+@contextmanager
+def warn_email(subject, address, do_send=True):
+    try:
+        yield
+    except Exception as e:
+        logger.info("Exception triggered. Emailing {}".format(address))
+        if do_send:
+            send_email(
+                subject=subject, body=traceback.format_exc(), to=address
+            )
+        raise (e)
+    finally:
+        pass
 
 
-def _test():
-    target = 'whc@ttic.edu'
-    send_email('ttic.mailer sending test', 'this is a test', target)
+# class ExceptionEmail:
+#     """
+#     A decorator that sends a warning email when catching
+#     exceptions. The caught exception will be re-raised.
+#     """
+#     def __init__(self, subject, address, do_send=True):
+#         """
+#         Args:
+#             subject: title of the email; some identifying metadata
+#             address: mail recipient address
+#         """
+#         self.subject = subject
+#         self.address = address
+#         self.do_send = do_send  # used to ensure only main process send email
 
-    @ExceptionEmail('ttic.mailer decorator test', target)
-    def bad_func():
-        return 1 / 0
+#     def __call__(self, f):
+#         @functools.wraps(f)
+#         def ret(*args, **kwargs):
+#             try:
+#                 f(*args, **kwargs)
+#             # except KeyboardInterrupt:
+#             #     pass  # do not send email on sigint
+#             except Exception as e:
+#                 logger.info("Exception triggered. Emailing {}".format(self.address))
+#                 if self.do_send:
+#                     send_email(
+#                         subject=self.subject,
+#                         body=traceback.format_exc(),
+#                         to=self.address
+#                     )
+#                 raise(e)
+#         return ret
 
-    bad_func()
+
+# def _test():
+#     target = 'whc@ttic.edu'
+#     send_email('ttic.mailer sending test', 'this is a test', target)
+
+#     @ExceptionEmail('ttic.mailer decorator test', target)
+#     def bad_func():
+#         return 1 / 0
+
+#     bad_func()
 
 
-if __name__ == '__main__':
-    _test()
+# if __name__ == '__main__':
+#     _test()
