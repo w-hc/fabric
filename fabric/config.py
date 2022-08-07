b37fa96c4106d4b7d21474bf8f3b261163f6f248
Haochen Wang <wanghaochen@uchicago.edu>
2022-08-06 21:35:02 -0500

    pydantic based config

---
+++ fabric/config.py
@@ -0,0 +1,32 @@
+from typing import List, Union
+from pydantic import BaseModel as _Base
+import yaml
+from pathlib import Path
+
+
+class BaseConf(_Base):
+    class Config:
+        validate_all = True
+        allow_mutation = False
+
+
+def SingleOrList(inner_type):
+    return Union[inner_type, List[inner_type]]
+
+
+def optional_load_config(fname="config.yaml"):
+    cfg = {}
+    conf_fname = Path.cwd() / fname
+    if conf_fname.is_file():
+        with conf_fname.open("r") as f:
+            raw = f.read()
+            print("loaded config\n ")
+            print(raw)  # yaml raw itself is well formatted
+            cfg = yaml.safe_load(raw)
+    return cfg
+
+
+def write_full_config(cfg_obj, fname="full_config.yaml"):
+    cfg = cfg_obj.dict()
+    with (Path.cwd() / fname).open("w") as f:
+        yaml.safe_dump(cfg, f, sort_keys=False)
